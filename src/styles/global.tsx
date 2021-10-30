22ab780b7c84731ee1f9014e4548466d9b9bc019
Matheus Santos Soares <mss.soares22@gmail.com>
2021-10-30 00:57:50 -0300

    fix: changes on global styles

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,94 +1,46 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  :root {
-    --background-color: #FAFAFA;
-    --main-color: #31575B;
-    --secondary-color: #197D67;
-    --description-primary-color: #969696;
-    --description-secondary-color: #707070;
-    --links-color: #1492E6;
-    --border-main-color: #D5D5D5;
-  
-    /* 1rem = 10px*/
-  }
-
+ 
   * {
     margin: 0;
     padding: 0;
   }
-
-  html, body {
+  :root{
+    --blue: #0CAACD;
+  }
+  html{
+        @media(max-width:1080px){
+            font-size: 93.75%; //15px
+        }
+        @media(max-width:720px){
+            font-size: 87.5%; //14px
+        }
+  }
+  html, body, #root {
     height: 100%;
-   
+    scroll-behavior: smooth;
+    --roboto-font-family: 'Roboto', sans-serif;
+
+    
+
+    font-family: var(--roboto-font-family);
   }
 
 
-  button:focus {
-    outline: none;
+  button{
+      user-select: none;
+      cursor: pointer;
   }
-
-  input, textarea, select {
-    background: transparent;
-    border: 1px solid var(--border-main-color);
-    border-radius: 6px;
-    padding: 5px 12px;
-    color: var(--color-input-text);
-  }
-
-  input, textarea, select:focus {
-    outline: none;
-  }
-
-  select {
-    border: 1px solid #A0A0A0;
-    color: #ACACAC;
-    padding: 1.2rem;
-  }
-  
-
-  a {
-    text-decoration: none;
-    margin-left: 1.3rem;
-    color: #1492E6;
-    font-size: 1.3rem;
-  }
-
-
-  .input-error {
-    border-color: #d9534f !important;
-  }
-
-
-  .custom-radio-input label input[type='radio']:after {
-    width: 18px;
-    height: 18px;
-    border-radius: 15px;
-    top: -2px;
-    left: -1px;
-    position: relative;
-    background-color: var(--color-background);
-    content: '';
-    display: inline-block;
-    visibility: visible;
-    border: 2px solid var(--color-border);
-  }
-
-  .custom-radio-input label input[type='radio']:checked:after {
-    width: 18px;
-    height: 18px;
-    border-radius: 15px;
-    top: -2px;
-    left: -1px;
-    position: relative;
-    background-color: var(--color-primary);
-    content: '';
-    display: inline-block;
-    visibility: visible;
-    border: 2px solid var(--color-border);
-  }
-
-  #root {
-    height: 100%;
-  }
+    [disabled]{
+        opacity:0.6;
+        cursor: not-allowed;
+    }
+    body, input, textarea, button{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+    }
+    h1, h1, h3, h4, h5, h6, strong{
+        font-weight:600;
+    }
 `;
