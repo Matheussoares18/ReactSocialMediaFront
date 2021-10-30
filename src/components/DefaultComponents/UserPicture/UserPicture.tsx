3c76c05081aadef457ed10fd9130318914c1a628
Matheus Santos Soares <mss.soares22@gmail.com>
2021-10-30 18:05:31 -0300

    feat: custom img tag

---
+++ src/components/DefaultComponents/UserPicture/UserPicture.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import userPhoto from '../../../assets/userPhoto.jpeg';
+
+interface UserPictureProps {
+  source?: string;
+  classname?: string;
+  onMouseEnter?: React.MouseEventHandler<HTMLImageElement> | undefined;
+  onClick?: React.MouseEventHandler<HTMLImageElement> | undefined;
+}
+export function UserPicture({
+  source,
+  classname,
+  onMouseEnter,
+  onClick,
+}: UserPictureProps) {
+  const [imageSource, setImageSource] = useState<string>('');
+
+  useEffect(() => {
+    if (source) {
+      setImageSource(source);
+    } else {
+      setImageSource(userPhoto);
+    }
+  }, [source]);
+  return (
+    <img
+      src={imageSource}
+      alt=" "
+      onClick={onClick}
+      onKeyPress={() => onClick}
+      loading="lazy"
+      role="presentation"
+      style={{ objectFit: `${source ? 'cover' : 'initial'}` }}
+      className={classname}
+      onMouseEnter={onMouseEnter}
+    />
+  );
+}
