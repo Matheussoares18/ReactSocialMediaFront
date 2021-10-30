42515f9d38fb773db06c4951323e7936ebcc2f4d
Matheus Santos Soares <mss.soares22@gmail.com>
2021-10-30 00:57:26 -0300

    feat: routes enum

---
+++ src/Routes/RoutesEnum.ts
@@ -0,0 +1,6 @@
+export enum PublicRoutes {
+  REGISTER = '/register',
+  REGISTER_USER_INFOS = '/user-infos',
+  REGISTER_BIRTH_DATE = '/user-birth-date',
+  REGISTER_PASSWORD = '/user-password',
+}
